1b287c07f7f4f9602e67998813c91b8084202226
dorotawejdman <dorota.wejdman@gmail.com>
2022-07-12 22:47:01 +0200

    Add mock plant data

---
+++ src/data/plants.js
@@ -0,0 +1,23 @@
+const plants = [
+  {
+    name: 'Amanda',
+    type: 'Calathea',
+    description: 'My favourite plant',
+    size: 'M',
+    pot: 1,
+  },
+  {
+    name: 'Chris',
+    type: 'Cactus',
+    description: 'Do not overwater!',
+    size: 'S',
+    pot: 2,
+  },
+  {
+    name: 'July',
+    type: 'Monstera',
+    description: 'She was so small when I bought it, about 10 cm.',
+    size: 'L',
+    pot: 3,
+  },
+];
