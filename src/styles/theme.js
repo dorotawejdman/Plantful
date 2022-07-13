981b37c0daa66a15f2aea16cfb374ed67ade5a34
Dorota Wejdman <dorota.wejdman@futurum.tech>
2022-07-13 17:01:01 +0200

    Add styles, theme and assets

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    accent1: '#b16078',
+    accent2: '#b16078',
+    primary: '#4b5e53',
+    primaryDark: '#4b5e53',
+    neutral1: '#b16078',
+  },
+  fontSize: {
+    l: '25px',
+    m: '18px',
+    s: '16px',
+  },
+};
