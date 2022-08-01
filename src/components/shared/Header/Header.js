6da2c53edc48d130badae1f5f9e79ca792548fa2
dorotawejdman <dorota.wejdman@gmail.com>
2022-08-01 23:11:55 +0200

    Change header into styled component

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledHeader = styled.div`
+  background-color: ${({ theme }) => theme.colors.primaryDark};
+  padding: 40px 0 10px 0;
+  text-align: center;
+  h2 {
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.colors.white};
+    font-size: ${({ theme }) => theme.fontSize.m};
+    font-weight: 500;
+  }
+`;
 
 const Header = ({ title }) => {
-  return <div>{title}</div>;
+  return (
+    <StyledHeader>
+      <h2>{title}</h2>
+    </StyledHeader>
+  );
 };
 
 Header.propTypes = {
