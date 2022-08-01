cd9a88230a01bc6d0851b60cc404066736e502c4
dorotawejdman <dorota.wejdman@gmail.com>
2022-08-01 23:12:50 +0200

    Add componnets to root view

--- src/views/Root.js
+++ src/views/Root.js
@@ -4,7 +4,10 @@ import Button from 'components/shared/Buttons/Button/Button';
 import IconButton from 'components/shared/Buttons/IconButton/IconButton';
 import GlobalStyles from 'styles/globalStyles';
 import { theme } from 'styles/theme';
-
+import FormInput from 'components/shared/Form/FormInput/FormInput';
+import Header from 'components/shared/Header/Header';
+import Label from 'components/shared/Label/Label';
+import LabelsList from 'components/molecules/UsersListItem/LabelsList';
 const Wrapper = styled.section`
   background-color: ${(props) => props.theme.colors.neutral1};
 `;
@@ -16,8 +19,11 @@ function Root() {
       <Wrapper>
         <UsersList />
         <Button text="add" color="primary"></Button>
-        <Button text="display" color="accent-1"></Button>
+        <Button text="display" color="accent2"></Button>
         <IconButton></IconButton>
+        <FormInput></FormInput>
+        <Header title="Create plant"></Header>
+        <LabelsList></LabelsList>
       </Wrapper>
     </ThemeProvider>
   );
