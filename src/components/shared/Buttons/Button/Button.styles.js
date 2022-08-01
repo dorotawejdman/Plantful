f89649ce3d4c70df82952835b0ce35dfffe3743f
dorotawejdman <dorota.wejdman@gmail.com>
2022-08-01 23:11:38 +0200

    Adjust global, theme and button styles

--- src/components/shared/Buttons/Button/Button.styles.js
+++ src/components/shared/Buttons/Button/Button.styles.js
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 export const StyledButton = styled.button`
   background-color: ${(props) => props.theme.colors[props.color]};
-  color: white;
-  border-radius: 5px;
-  padding: 8px 20px;
+  color: ${({ theme }) => theme.colors.neutral1};
+  border-radius: 4px;
+  text-transform: uppercase;
+  padding: 9px 63px;
   margin: 10px;
   border: none;
   font-size: 1.2rem;
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -3,7 +3,9 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
 
 @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700;800&display=swap');
 
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
 html{
   box-sizing: border-box;
 }
@@ -14,7 +16,9 @@ html{
 
 * {
   font-family: 'Nunito', sans-serif;
+  font-family: 'Roboto', sans-serif;
 }
+
 
 `;
 
