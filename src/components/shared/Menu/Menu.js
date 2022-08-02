4d68a418dbd42ae9f48dfb9cda8fed8a373a67fa
dorotawejdman <dorota.wejdman@gmail.com>
2022-08-02 22:40:30 +0200

    Create Menu component

---
+++ src/components/shared/Menu/Menu.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import MenuItem from './MenuItem';
+
+const StyledMenu = styled.ul`
+  background-color: ${({ theme }) => theme.colors.neutral4};
+  position: absolute;
+  bottom: ${({ theme }) => theme.height.nav};
+  width: 100%;
+  text-align: center;
+  box-shadow: 0px 0px 10px 2px rgba(146, 144, 141, 0.25);
+`;
+const Menu = () => {
+  return (
+    <StyledMenu>
+      <MenuItem>Plant</MenuItem>
+      <MenuItem>Activity</MenuItem>
+    </StyledMenu>
+  );
+};
+
+Menu.propTypes = {};
+
+export default Menu;
---
+++ src/components/shared/Menu/MenuItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { ReactComponent as Line } from 'assets/Line.svg';
+import { flexCenter } from 'styles/mixins';
+
+const StyledMenuItem = styled.div`
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.colors.black2};
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  height: 73px;
+  ${flexCenter}
+`;
+
+const MenuItem = ({ children }) => {
+  return (
+    <>
+      <StyledMenuItem>${children}</StyledMenuItem>
+      <Line></Line>
+    </>
+  );
+};
+
+MenuItem.propTypes = {};
+
+export default MenuItem;
