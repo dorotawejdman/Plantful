66d474afcae8c1872433463c2954639ac7daf004
Dorota Wejdman <dorota.wejdman@futurum.tech>
2022-08-02 10:52:44 +0200

    Style iconButton use react icons

--- src/components/shared/Buttons/IconButton/IconButton.js
+++ src/components/shared/Buttons/IconButton/IconButton.js
@@ -2,19 +2,28 @@ import React from 'react';
 // import addIcon from 'assets/icons/add.svg';
 import { ReactComponent as AddIcon } from 'assets/icons/add.svg';
 import styled from 'styled-components';
+import { TbPlant2 } from 'react-icons/tb';
 
 const StyledIconButton = styled.button`
-  width: 50px;
+  width: 40px;
+  height: 40px;
+  border-radius: ${({ theme }) => theme.border.radius};
+  border: none;
+  background-color: ${(props) => props.theme.colors[props.color]};
+  display: flex;
+  justify-content: center;
+  align-items: center;
   svg {
-    height: 10px;
+    height: 20px;
+    width: 20px;
+    color: ${({ theme }) => theme.colors.neutral1};
   }
 `;
-
+//Add choosing different icons based on parameter? Or an icon as a children?
 const IconButton = (props) => {
   return (
-    <StyledIconButton>
-      {/* <img src={addIcon}></img> */}
-      <AddIcon></AddIcon>
+    <StyledIconButton color={props.color}>
+      <TbPlant2 />
     </StyledIconButton>
   );
 };
