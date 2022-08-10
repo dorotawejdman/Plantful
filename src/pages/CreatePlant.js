a2b203923ca721fee323151b97760b6102a5434f
Dorota Wejdman <dorota.wejdman@futurum.tech>
2022-08-10 13:19:52 +0200

    Create createPlant page content

--- src/pages/CreatePlant.js
+++ src/pages/CreatePlant.js
@@ -1,5 +1,13 @@
 import React from 'react';
-
+import Button from 'components/shared/Buttons/Button/Button';
+import PlantForm from 'components/smart/PlantForm';
+import Header from 'components/shared/Header/Header';
 export const CreatePlant = () => {
-  return <div>CreatePlant</div>;
+  return (
+    <>
+      <Header>Create plant</Header>
+      <PlantForm></PlantForm>
+      <Button color="primary">add</Button>
+    </>
+  );
 };
